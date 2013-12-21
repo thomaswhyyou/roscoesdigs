2b0c0015944e6bf1576cdf0070b0f4fc8c06b0e9
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-12-21 18:12:50 -0500

    preparing to test-deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn roscoesdigs.wsgi
--- roscoesdigs/settings/prod_settings.py
+++ roscoesdigs/settings/prod_settings.py
@@ -1,1 +1,19 @@
 from .base_settings import *
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'public'
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
